08f3640f6b704b24e5c6bd06fa8696a05b56e38b
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-11-08 20:47:24 +0100

    Initial Commit

---
+++ Collection View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection View
+//
+//  Created by Macbook Air 2017 on 8. 11. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
